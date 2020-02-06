3d0686bedc846a110e68e383662a7be35ab82415
nobowned <lancerbarnes@gmail.com>
2020-02-05 17:24:27 -0700

    Hopefully this is the last move/rename done with these silly template thingys.

--- server-examples/create-linux-server.sh
+++ server-examples/create-linux-server.sh
@@ -2,7 +2,7 @@ mkdir linux-server
 mkdir linux-server/main
 mkdir linux-server/main/dmscripts
 
-cp ../server-files/main/*.cfg linux-server/main
-cp ../server-files/main/dmscripts/* linux-server/main/dmscripts
+cp server-files/main/*.cfg linux-server/main
+cp server-files/main/dmscripts/* linux-server/main/dmscripts
 
 echo './wolfded.x86 +exec deathmatch.cfg' > linux-server/start.sh
--- server-examples/create-windows-server.bat
+++ server-examples/create-windows-server.bat
@@ -2,7 +2,7 @@ mkdir windows-server
 mkdir windows-server\main
 mkdir windows-server\main\dmscripts
 
-copy ..\server-files\main\*.cfg windows-server\main
-copy ..\server-files\main\dmscripts windows-server\main\dmscripts
+copy server-files\main\*.cfg windows-server\main
+copy server-files\main\dmscripts windows-server\main\dmscripts
 
 echo WolfDED.exe +exec deathmatch.cfg > windows-server\start.bat
